df7f0784976b54b60d0ddd73b8794959630a39bb
AnnaSolozobova <solozobovaanna5@gmail.com>
2022-09-17 04:02:26 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 2100;
+
+        int mile = 20;
+
+        int numberOfMiles = ticketPrice / mile;
+
+        System.out.println(numberOfMiles + " бонусных миль");
+    }
+}
